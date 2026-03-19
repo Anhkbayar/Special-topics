3d0c5463c9371a8f352523285220e520774190af
anhkbayar <zanhaa56@gmail.com>
2026-03-19 23:45:43 +0800

    minor changes on lab2

--- lab02/havsralt3/src/main/java/com/lab23/PopularityService.java
+++ lab02/havsralt3/src/main/java/com/lab23/PopularityService.java
@@ -1,17 +1,17 @@
 package com.lab23;
 
-import java.util.List;
 import java.util.Date;
+import java.util.List;
 
 public class PopularityService {
-    private PopularityRepository popularityRepository;
+  private PopularityRepository popularityRepository;
 
-    public void update(String category, List<Product> products){
-        PopularityData data = new PopularityData();
-        data.setCategory(category);
-        data.setProductCount(products.size());
-        data.setLastAccessed(new Date());
+  public void update(String category, List<Product> products) {
+    PopularityData data = new PopularityData();
+    data.setCategory(category);
+    data.setProductCount(products.size());
+    data.setLastAccessed(new Date());
 
-        popularityRepository.save(data);
-    }
+    popularityRepository.save(data);
+  }
 }
