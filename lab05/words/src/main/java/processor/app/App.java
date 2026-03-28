2a803b5f3376bf18b6adfa33d7b2112962598213
anhkbayar <zanhaa56@gmail.com>
2026-03-28 19:05:44 +0800

    fixed resources

--- lab05/words/src/main/java/processor/app/App.java
+++ lab05/words/src/main/java/processor/app/App.java
@@ -1,39 +1,44 @@
 package processor.app;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
-
+import java.util.stream.Collectors;
 import processor.app.sorters.AlphabeticalSorter;
 import processor.app.sorters.CapitalFirstSorter;
-import processor.app.sorters.ReverseAlphabeticalSorter;
 import processor.app.sorters.RemoveDuplicate;
-/**
- * Parser
- */
+import processor.app.sorters.ReverseAlphabeticalSorter;
+
+/** Parser */
 public class App {
-    public static void main(String[] args) throws IOException{
-        if(args.length < 2){
-            System.out.println("Usage: <file> <options>");
-            System.out.println("Options: -asc -desc -unique -capfirst");
-        }
+  public static void main(String[] args) throws IOException {
+    if (args.length < 1) {
+      System.out.println("Usage: <options>");
+      System.out.println("Options: -asc -desc -unique -capfirst");
+    }
 
-        String filename = args[0];
-        String option = args[1];
+    String option = args[0];
 
-        List<String> words = Files.readAllLines(Path.of(filename));
-        IWordProcessor processor = switch(option){
-            case "-asc" -> new AlphabeticalSorter();
-            case "-desc" -> new ReverseAlphabeticalSorter();
-            case "-unique" -> new RemoveDuplicate();
-            case "-capfirst" -> new CapitalFirstSorter();
-            default -> throw new IllegalArgumentException("Unknown option: "+ option);
+    List<String> words;
+    try (InputStream in = App.class.getResourceAsStream("/words.txt")) {
+      if (in == null) throw new IOException("words.txt not found");
+      words = new BufferedReader(new InputStreamReader(in)).lines().collect(Collectors.toList());
+    }
+
+    IWordProcessor processor =
+        switch (option) {
+          case "-asc" -> new AlphabeticalSorter();
+          case "-desc" -> new ReverseAlphabeticalSorter();
+          case "-unique" -> new RemoveDuplicate();
+          case "-capfirst" -> new CapitalFirstSorter();
+          default -> throw new IllegalArgumentException("Unknown option: " + option);
         };
 
-        List<String> result = processor.process(words);
+    List<String> result = processor.process(words);
 
-        result.forEach(System.out::println);
-        // System.out.println("Hello World!");
-    }
+    result.forEach(System.out::println);
+    // System.out.println("Hello World!");
+  }
 }
