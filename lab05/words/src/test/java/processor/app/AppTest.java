6650fd95c27bea35123d4a9f536ee30e50856a8c
anhkbayar <zanhaa56@gmail.com>
2026-03-28 19:15:16 +0800

    added tests

--- lab05/words/src/test/java/processor/app/AppTest.java
+++ lab05/words/src/test/java/processor/app/AppTest.java
@@ -1,19 +1,50 @@
 package processor.app;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import processor.app.sorters.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest {
 
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
-    }
+  @Test
+  void testAlphabeticalSort() {
+    List<String> words = Arrays.asList("banana", "Apple", "orange");
+    AlphabeticalSorter sort = new AlphabeticalSorter();
+    List<String> result = sort.process(words);
+    assertEquals("Apple", result.get(0));
+    assertEquals("banana", result.get(1));
+    assertEquals("orange", result.get(2));
+  }
+
+  @Test
+  void testCapitalSort(){
+    List<String> words = Arrays.asList("apple", "bold", "Ankle");
+    CapitalFirstSorter sort = new CapitalFirstSorter();
+    List<String> result = sort.process(words);
+    assertEquals("Ankle", result.get(0));
+    assertEquals("apple", result.get(1));
+    assertEquals("bold", result.get(2));
+  }
+
+  @Test
+  void ReverseAlphabeticalSorter(){
+    List<String> words = Arrays.asList("array", "high", "zebra");
+    ReverseAlphabeticalSorter sort = new ReverseAlphabeticalSorter();
+    List<String> result = sort.process(words);
+    assertEquals("zebra", result.get(0));
+    assertEquals("high", result.get(1));
+    assertEquals("array", result.get(2));
+  }
+
+  @Test
+  void testRemoveDuplicates() {
+    List<String> words = Arrays.asList("apple", "apple", "banana");
+    RemoveDuplicate processor = new RemoveDuplicate();
+    List<String> result = processor.process(words);
+    assertEquals(2, result.size());
+  }
 }
